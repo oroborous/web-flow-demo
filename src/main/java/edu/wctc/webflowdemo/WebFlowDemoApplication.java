f3ada0ddf9c93deccf806321d3f31f7f1ab83e3b
BIT Instructor <bitinstructor@bitnet.wctc.edu>
2021-11-04 15:35:35 -0500

    Initial commit

---
+++ src/main/java/edu/wctc/webflowdemo/WebFlowDemoApplication.java
@@ -0,0 +1,13 @@
+package edu.wctc.webflowdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFlowDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFlowDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/wctc/webflowdemo/WebFlowDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.wctc.webflowdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebFlowDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
